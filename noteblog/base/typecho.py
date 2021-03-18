dbcad8fea91645d370d0b084379caff245e2c07c
niuliangtao <niuliangtao@qq.com>
2021-03-18 11:39:36 +0800

    add

--- noteblog/base/typecho.py
+++ noteblog/base/typecho.py
@@ -1,6 +1,7 @@
 from noteblog.base.core import PublishBase
 from noteblog.base.meta import Cate, Page
 from noteblog.blog.typecho import Typecho
+from noteblog.blog.typecho.models import Category as TypeCate
 from noteblog.blog.typecho.models import Page as TypePage
 
 
@@ -9,8 +10,22 @@ class TypechoPB(PublishBase):
         super(TypechoPB, self).__init__(*args, **kwargs)
         self.typecho: Typecho = typecho
 
-    def page_transform(self, page: TypePage) -> TypePage:
-        return page
+    @staticmethod
+    def page_transform(page: Page) -> TypePage:
+        type_page = TypePage(
+            title=page.title,
+            description=page.description,
+            # categories=[],
+        )
+        return type_page
+
+    @staticmethod
+    def cate_transform(cate: Cate) -> TypeCate:
+        type_cate = TypeCate(
+            name=cate.cate_name,
+            parent=cate.parent_id
+        )
+        return type_cate
 
     def get_pages(self, nums=10, *args, **kwargs):
         return self.typecho.get_pages()
@@ -22,24 +37,22 @@ class TypechoPB(PublishBase):
         return self.typecho.new_page(self.page_transform(page), True)
 
     def edit_page(self, page_id, page: Page, *args, **kwargs):
-        pass
+        return self.typecho.edit_page(self.page_transform(page), page_id, True)
 
     def del_page(self, page_id, *args, **kwargs):
         pass
 
     def get_cates(self, nums=10, *args, **kwargs):
-        pass
+        return self.typecho.get_categories()
 
     def get_cate(self, cate_id, *args, **kwargs):
-        pass
+        return self.typecho.get_categories()
 
     def new_cate(self, cate: Cate, *args, **kwargs):
-        pass
+        return self.typecho.new_category(self.cate_transform(cate), parent_id=cate.parent_id)
 
     def edit_cate(self, cate_id, cate: Cate, *args, **kwargs):
         pass
 
     def del_cate(self, cate_id, *args, **kwargs):
         pass
-
-
