860dd7573019863ceead40f182d26f47bd592b0f
niuliangtao <niuliangtao@qq.com>
2021-03-23 19:39:54 +0800

    add

--- example/publish.py
+++ example/publish.py
@@ -10,5 +10,5 @@ blog = BlogManage('/root/workspace/content/publish',
 rpc_url = read_secret(cate1='blog', cate2='typecho', cate3='rpc_url')
 username = read_secret(cate1='blog', cate2='typecho', cate3='username')
 password = read_secret(cate1='blog', cate2='typecho', cate3='password')
-blog.local_scan()
+#blog.local_scan()
 blog.publish_typecho(rpc_url=rpc_url, username=username, password=password)
