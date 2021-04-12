196f54512dd645e00edb6ab9f1be0ecbd3fd69d6
niuliangtao <niuliangtao@qq.com>
2021-04-12 20:11:22 +0800

    Revert "add"
    This reverts commit 77001523d3bb29b432438214b435350b447c6d2b.

---
+++ example/publish.py
@@ -0,0 +1,14 @@
+from noteblog.publish.core import BlogManage
+from notetool.tool.secret import read_secret
+
+# pip install  git+https://gitee.com/notechats/noteblog.git
+# pip install  git+https://gitee.com/notechats/notetool.git
+
+blog = BlogManage('/root/workspace/content/publish',
+                  db_path='/root/workspace/content/blog.db')
+
+rpc_url = read_secret(cate1='blog', cate2='typecho', cate3='rpc_url')
+username = read_secret(cate1='blog', cate2='typecho', cate3='username')
+password = read_secret(cate1='blog', cate2='typecho', cate3='password')
+blog.local_scan()
+blog.publish_typecho(rpc_url=rpc_url, username=username, password=password)
